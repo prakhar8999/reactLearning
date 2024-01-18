ee1d299c8f05cfa3b209d1a17e6f5a6d36248b19
prakhar <workprakhar@gmail.com>
2024-01-19 00:32:00 +0530

    Some change in footer

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/header/header'
+import Header from './components/header/Header'
 import { Outlet } from 'react-router-dom'
-import Footer from './components/footer/footer'
+import Footer from './components/footer/Footer'
 
 
 function Layout() {
