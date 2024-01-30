06b3a821f3168ec5657a02ae87397c761604055b
cipher <prakhartwr8999@gmail.com>
2024-01-31 00:36:38 +0530

    changes for Todo

--- toDoProj/src/App.jsx
+++ toDoProj/src/App.jsx
@@ -1,15 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { toDoContextProvider } from './context/toDoContext'
 import './App.css'
+import ToDoForm from './components/ToDoForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addToDo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+  }
+
+  const updateToDo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteToDo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed : !prevTodo.completed} : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
-    <>
-      <h1 className=' text-3xl font-bold underline'>Hello To-Do App</h1>
-    </>
+    <toDoContextProvider value={{todos, addToDo, updateToDo, deleteToDo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            <ToDoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </toDoContextProvider>
   )
 }
 
--- toDoProj/src/context/toDoContext.js
+++ toDoProj/src/context/toDoContext.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useContext } from 'react'
 
-const toDoContext = React.createContext()
+const toDoContext = React.createContext({
+    todos : [
+        {
+            id : 1,
+            todo : " ToDo Msg",
+            completed : false
+        }
+    ],
+    addToDo : (todo) => {},
+    updateToDo : (id , todo) => {},
+    deleteToDo : (id) => {},
+    toggleComplete : (id) => {}
+})
 
 const toDoContextProvider = toDoContext.Provider
-const toDoContextConsumer = toDoContext.Consumer
 
-export {toDoContextProvider, toDoContextConsumer}
+const useTodo = () => {
+    return useContext(toDoContext)
+}  
+
+export {toDoContext, toDoContextProvider, useTodo}
