028db8dd4f446f8fd0362da56de0dc5eb53cf2c9
prakhar <workprakhar@gmail.com>
2024-02-17 02:18:22 +0530

    Todo With Redux Completed

--- reduxToolKitProj/src/App.jsx
+++ reduxToolKitProj/src/App.jsx
@@ -1,14 +1,22 @@
 import AddTodo from './components/AddToDo'
 import './App.css'
 import Todos from './components/Todos'
+import { useSelector } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit';
 
 function App() {
+  const todos = useSelector(state => state.todos)
 
   return (
     <>
       <h1 className=' text-3xl font-bold '>Learn React Redux ToolKit</h1>
       <AddTodo />
-      <Todos />
+      <div>Todos</div>
+      {todos.map((todo) => (
+      <div key={nanoid()} className='w-full'>
+              <Todos todo={todo} />
+      </div>
+      ))}
     </>
   )
 }
--- reduxToolKitProj/src/components/Todos.jsx
+++ reduxToolKitProj/src/components/Todos.jsx
@@ -1,29 +1,50 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todos/todoSlice'
+import {removeTodo, updateTodo} from '../features/todos/todoSlice'
+import { nanoid } from '@reduxjs/toolkit'
 
-function Todos() {
+function Todos({todo}) {
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
 
+    const [txt,setTxt] = useState(todo.text)
+    const [isEditable,setisEditable] = useState(false)
+
+    const editTodo = () => {
+      if(isEditable === false){
+      setisEditable(true)
+      }
+      else{
+        dispatch(updateTodo({id : todo.id,text : txt}))
+        setisEditable(false)
+        
+    }
+  }
+
   return (
     <>
-    <div>Todos</div>
     <ul className="list-none">
-        {todos.map((todo) => (
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded relative"
             key={todo.id}
           >
-            <div className='text-white'>{todo.text}</div>
+            <div className='text-white'>
+              <input type = 'text' value={txt} onChange={(e) => setTxt(e.target.value)} readOnly={!isEditable} 
+              className= {`${isEditable ? " bg-black" : "bg-zinc-800"}`}/>
+            </div>
             <button
              onClick={() => dispatch(removeTodo(todo.id))}
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md absolute right-2"
             >
               Remove
             </button>
+            <button
+             onClick={() => editTodo()}
+              className="text-white bg-blue-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600 rounded text-md absolute right-28"
+            >
+              {isEditable ? 'Update' : 'Edit'}
+            </button>
           </li>
-        ))}
       </ul>
     </>
   )
