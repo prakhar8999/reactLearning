7fe74c0fefdac71b36ce754b700a8fd93be867ed
prakhar <workprakhar@gmail.com>
2024-02-16 21:31:09 +0530

    Redux Tool Kit First Proj

---
+++ reduxToolKitProj/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from './components/AddToDo'
+import './App.css'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-3xl font-bold '>Learn React Redux ToolKit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
